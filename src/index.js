c7591a6220786b0bbd5ab50fa59f6f3844702fdb
mdm106 <mads.mcmahon@googlemail.com>
2020-06-08 16:29:55 +0100

    01-10-Challenge: Changed so that server alternates every two serves if both scores get to 20

--- src/index.js
+++ src/index.js
@@ -13,7 +13,15 @@ const initial = {
 
 const player1 = state => ({ ...state, player1: state.player1 + 1 });
 const player2 = state => ({ ...state, player2: state.player2 + 1 });
-const server = state => ({ ...state, serving: (state.player1 + state.player2) % 5 === 0 ? !state.serving : state.serving })
+const server = state => {
+  let change = 5;
+  if(state.player1 >= 20 && state.player2 >= 20){
+    change = 2;
+  }
+  return {...state, 
+          serving: (state.player1 + state.player2) % change === 0 ? !state.serving : state.serving}
+}
+
 const winning = state => {
   let win = "";
   if(state.player1 >= 21 && state.player2 <= (state.player1 - 2)) {
