c040f0da2408d58b6d3f91e7dea6eb24ca5115de
mdm106 <mads.mcmahon@googlemail.com>
2020-06-08 13:05:14 +0100

    Store created and subscribed to with a render function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const initial = {
+  player1: 0,
+  player2: 0,
+};
+
+const store = createStore(
+  reducer,
+  initial,
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// we update subscribe to call the ReactDOM.render // method whenever the state changes
+const render = () => {
+  let state = store.getState();
+  // pass in state.value as a value prop
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root") 
+    );
+  };
+
+  store.subscribe(render); // render when state changes render(); // render when page loads using initial state
+
+  render(); // render when page loads using initial state
