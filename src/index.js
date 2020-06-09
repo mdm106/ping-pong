517cef72c35c0e79ddb049db6d91a17bc2a85899
mdm106 <mads.mcmahon@googlemail.com>
2020-06-09 14:28:29 +0100

    Provider added to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 import initial from '../src/data/initial';
 import reducer from '../src/data/reducer';
 import store from '../src/data/store';
+
+import { Provider } from 'react-redux';
 
 // we update subscribe to call the ReactDOM.render // method whenever the state changes
 const render = () => {
@@ -11,16 +14,18 @@ const render = () => {
   // pass in state.value as a value prop
   ReactDOM.render(
     <React.StrictMode>
-      <App
-        player1={player1}
-        player2={player2}
-        serving={serving}
-        winner={winner}
-        gameHistory={gameHistory}
-        handleIncrementP1={ () => store.dispatch({ type: "INCREMENT_P1" }) } 
-        handleIncrementP2={ () => store.dispatch({ type: "INCREMENT_P2" }) }
-        handleReset={ () => store.dispatch({ type: "RESET" }) }
-      />
+      <Provider store={ store }>
+        <App
+          player1={player1}
+          player2={player2}
+          serving={serving}
+          winner={winner}
+          gameHistory={gameHistory}
+          handleIncrementP1={ () => store.dispatch({ type: "INCREMENT_P1" }) } 
+          handleIncrementP2={ () => store.dispatch({ type: "INCREMENT_P2" }) }
+          handleReset={ () => store.dispatch({ type: "RESET" }) }
+        />
+      </Provider>
     </React.StrictMode>,
     document.getElementById("root") 
     );
