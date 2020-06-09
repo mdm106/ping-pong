478dc1aaa09762086a73c82c179e0183f87bbb7b
mdm106 <mads.mcmahon@googlemail.com>
2020-06-09 15:58:27 +0100

    index.js updated to remove render method and use Provider only

--- src/index.js
+++ src/index.js
@@ -6,26 +6,16 @@ import store from '../src/data/store';
 
 import { Provider } from 'react-redux';
 
-// we update subscribe to call the ReactDOM.render // method whenever the state changes
-const render = () => {
-  let { winner, gameHistory } = store.getState();
-  // pass in state.value as a value prop
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={ store }>
-        <App
-          winner={winner}
-          gameHistory={gameHistory}
-          handleIncrementP1={ () => store.dispatch({ type: "INCREMENT_P1" }) } 
-          handleIncrementP2={ () => store.dispatch({ type: "INCREMENT_P2" }) }
-          handleReset={ () => store.dispatch({ type: "RESET" }) }
-        />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById("root") 
-    );
-  };
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={ store }>
+      <App
+        handleIncrementP1={ () => store.dispatch({ type: "INCREMENT_P1" }) } 
+        handleIncrementP2={ () => store.dispatch({ type: "INCREMENT_P2" }) }
+        handleReset={ () => store.dispatch({ type: "RESET" }) }
+      />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root") 
+  );
 
-  store.subscribe(render); // render when state changes render(); // render when page loads using initial state
-
-  render(); // render when page loads using initial state
