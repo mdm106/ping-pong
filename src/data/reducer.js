64dff11fe15eb8e97bcf1855af98521529a391de
mdm106 <mads.mcmahon@googlemail.com>
2020-06-10 16:10:37 +0100

    Reducer updated so that winningScore and alternate serves are taken from state and so that only the initial scores and server are reset when reset button clicked

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -3,40 +3,49 @@ import initial from './initial';
 const player1 = state => ({ ...state, player1: state.player1 + 1 });
 const player2 = state => ({ ...state, player2: state.player2 + 1 });
 const server = state => {
-  let change = 5;
-  if(state.player1 >= 20 && state.player2 >= 20){
+  let change = state.alternate;
+  let deuce = state.winningScore - 1
+
+  let { player1, player2, serving } = state;
+
+  if(player1 >= deuce && player2 >= deuce){
     change = 2;
   }
   return {...state, 
-          serving: (state.player1 + state.player2) % change === 0 ? !state.serving : state.serving}
+          serving: (player1 + player2) % change === 0 ? !serving : serving}
 }
 
 const winning = state => {
   let win = "";
-  if(state.player1 >= 21 && state.player2 <= (state.player1 - 2)) {
+  let { winningScore, player1, player2 } = state;
+  if(player1 >= winningScore && player2 <= (player1 - 2)) {
     win="1"
-  } else if (state.player2 >= 21 && state.player1 <= (state.player2 - 2)){
+  } else if (player2 >= winningScore && player1 <= (player2 - 2)){
     win="2"
   }
   return {...state, winner: win}
 }
 
-const updateHistory = state => {
+const updateHistory = ({ winner, gameHistory, player1, player2, player1Name, player2Name, winningScore, alternate }) => {
   return {
     ...initial,
-    gameHistory: state.winner === "" ? state.gameHistory :
-      [...state.gameHistory,
+    gameHistory: winner === "" ? gameHistory :
+      [...gameHistory,
         {
             player_1: {
-              score: state.player1,
-              won: state.winner === "1"
+              score: player1,
+              won: winner === "1"
             },
             player_2: {
-              score: state.player2,
-              won: state.winner === "2"
+              score: player2,
+              won: winner === "2"
             }
         }
-      ] 
+      ],
+    player1Name,
+    player2Name,
+    winningScore,
+    alternate 
   }
 }
 
